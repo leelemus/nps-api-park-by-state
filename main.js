aee92abee16dd2075915eda2f471a9dd3ec53986
Lee Lemus <leelemus@gmail.com>
2019-06-17 22:21:03 -0500

    styled form a bit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const apiKey = "OA0UJvIMzPpURsHVpceC3w9MmEyWTLFSCNfdz2vE"; 
+const apiKey = 'OA0UJvIMzPpURsHVpceC3w9MmEyWTLFSCNfdz2vE'; 
 const searchURL = 'https://developer.nps.gov/api/v1/parks';
 
 
@@ -10,34 +10,51 @@ function formatStateQuery(stateValues) {
     return stateQuery.join(",");
 }
   
-  function displayResults(responseJson, maxResults) {
+  function displayResults(eachPark) {
     // if there are previous results, remove them
-    console.log(responseJson);
-
+    $('#parkResults').append(`
+        <li><h3>${eachPark.fullName}</h3><p><strong>Description: </strong>${eachPark.description}</p><p><strong>URL: </strong>${eachPark.url}</p></li>
+    `);
   };
 
-function getResults(stateValues, maxResults=10) {
+function getResults(stateValues, maxResults) {
 
-    const stateList = formatStateQuery(stateValues);
-    const stateRequest = "stateCode="+stateList;
-    
-    const url = searchURL + '?' + stateRequest;
-    
+    const stateRequest = 'stateCode=' + formatStateQuery(stateValues);
+    const totalResults = 'limit=' + (maxResults - 1);
+    const apiUrl = 'api_key=' + apiKey;
+    const url = searchURL + '?' + stateRequest + '&' + totalResults + '&' + apiUrl;
     console.log(url);
+
+    /* This version give me a 403 - need to look into adding headers
+    
+    const url = searchURL + '?' + stateRequest + '&' + totalResults;
     
     const options = {
         headers: new Headers({
           "X-Api-Key": apiKey})
-      };
-    
+    };
+
     fetch(url, options)
         .then(response => {
-          if (response.ok) {
-            return response.json();
-          }
-          throw new Error(response.statusText);
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
         })
-        .then(responseJson => displayResults(responseJson, maxResults))
+        .then(responseJson => responseJson.data.forEach(displayResults))
+        .catch(err => {
+          $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
+    */
+
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => responseJson.data.forEach(displayResults))
         .catch(err => {
           $('#js-error-message').text(`Something went wrong: ${err.message}`);
         });
@@ -47,7 +64,8 @@ function parkGenerator() {
     $('#js-parkForm').submit(event => {
         event.preventDefault();
         const stateValues = $('#js-parkForm').serializeArray();
-        const maxResults = $('#js-max-results').val();
+        const maxResults = $('#js-maxResults').val();
+        $('#parkResults').empty();
         getResults(stateValues, maxResults);
     });
 }
